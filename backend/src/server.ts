60dad8702037f9e71e230509d5aa463501763cc5
Cherokee Walters <cherokeewalters162@gmail.com>
2023-05-06 14:55:34 -0400

    eod

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import * as path from "path";
 
 const app = express();
 
@@ -17,6 +18,7 @@ type TTile = {
     type: TileType
     health_cost: number
     move_cost: number
+    symbol: string
 }
 
 function createTile(): TTile {
@@ -40,39 +42,53 @@ class BlankTile implements TTile {
     type = TileType.Blank
     health_cost = 0
     move_cost = -1
+    symbol = '.'
 }
 
 class SpeederTile implements TTile {
     type = TileType.Blank
     health_cost = -5
     move_cost = 0
+    symbol = 'S'
 }
 
 class LavaTile implements TTile {
     type = TileType.Lava
     health_cost = -50
     move_cost = -10
+    symbol = 'L'
 }
 
 class MudTile implements TTile {
     type = TileType.Mud
     health_cost = -10
     move_cost = -5
+    symbol = 'M'
 }
 
 class StartTile implements TTile {
     type = TileType.Start
     health_cost = 0
     move_cost = -1
+    symbol = 'H'
 }
 
 class EndTile implements TTile {
     type = TileType.End
     health_cost = 0
     move_cost = -1
+    symbol = 'Z'
 }
 
-function generateMap(size: number): TTile[][] {
+function generateMap(difficulty: string): TTile[][] {
+    let size = 50
+    switch (difficulty) {
+        case "easy": size = 30; break
+        case "medium": size = 40; break
+        case "hard": size = 50; break
+        default: size = 50
+    }
+
     let new_array: TTile[][] = []
     for (let row = 0; row < size; row++){
         new_array.push([])
@@ -115,48 +131,55 @@ function getStartLocation(): TPos {
     return {x: 0, y: y}
 }
 
+function createPlayer(difficulty: string): Player {
+
+    return new Player(health, moves, location)
+}
+// ROUTE SECTION
+
 app.get('/', async (_req, res) => {
-    res.send("<h1>I'm alive!</h1>")
+    res.sendFile("./index.html", {root: path.join(__dirname, '.')})
 });
 
-app.get('/generate-map/:size', (req, res) => {
-    try {
-        const size: number = Number.parseInt(req.params.size)
-        if (size < 5) {
-            res.status(401).json({ error: "not a number 5 or greater."})
-        }
-        const world = generateMap(size)
-        app.locals.world = world
-        res.json({map: app.locals.world, error: ''});
-    } catch {
-        res.status(401).json({ error: "not a number 5 or greater."})
-    }
-})
-
-app.get('/create-player/:health/:moves/', (req, res) => {
-    let health: number = 200
-    let moves: number = 450
-    try {
-        health = Number.parseInt(req.params.health)
-        if (health < 50 ) {
-            res.status(401).json({ error: "Not a number 50 or greater"})
-        }
-    } catch {
-        res.status(401).json({ error: "not a number 50 or greater."})
-    }
+app.get('/new-game/:difficulty', (req, res) => {
+    let difficulty= req.params.difficulty.toLowerCase()
 
     try {
-        moves = Number.parseInt(req.params.moves)
-        if (moves < 100 ) {
-            res.status(401).json({ error: "Not a number 100 or greater"})
+
+        if (difficulty == "easy" || difficulty == "medium" || difficulty == "hard") {
+            app.locals.world = generateMap(difficulty)
+            // app.locals.player = createPlayer()
+            res.status(200).send("<h1>Game created successfully!</h1>" +
+                "<h2>Please check /help for assistance on how to play!</h2>")
+        }
+        else {
+            res.status(400).send("<h1>Please only use easy/medium/hard for the difficulty.</h1>")
         }
     } catch {
-        res.status(401).json({ error: "not a number 100 or greater."})
+        res.status(500).send("<h1>Game failed to be created!</h1>" +
+        "<h2>Please contact the server administrator</h2>")
     }
-    const start_location = getStartLocation();
-    app.locals.player = new Player(health, moves, start_location);
-    res.status(200).json({player: app.locals.player, error: ''})
+});
+
+app.get('/move/:direction', (_req, _res) => {
+
+});
+
+app.get('/player', (_req, _res) => {
+
+});
+
+app.get('/map', (_req, _res) => {
+
+});
+
+app.get('/help', (_req, _res) => {
+
 })
+
+app.get('/world-status', (_req, _res) => {
+
+});
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
