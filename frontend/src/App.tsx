985dd625a53e9b53183de3bddb56ed7cfffd5f4f
Cherokee Walters <cherokeewalters162@gmail.com>
2023-06-23 23:47:22 -0400

    CRA-typescript compelte

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p className={"bg-green-500 border-8 border-red-600"}>This is my custom paragraph!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
